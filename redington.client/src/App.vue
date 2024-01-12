1060aab7cd314dec1367385e4f6c934545e61523
Reivynn Chiang <reivynn.chiang@gmail.com>
2024-01-12 15:45:32 +0000

    Added text to describe the calculations

--- redington.client/src/App.vue
+++ redington.client/src/App.vue
@@ -1,26 +1,47 @@
 <template>
-    <SelectButton v-model="value" :options="options" aria-labelledby="basic" />
+    <SelectButton v-model="value" :options="options" aria-labelledby="basic" /> 
+    <br />
+    <Card>
+        <template #title>{{value}}</template>
+        <template #content>
+            <p id="combined" v-if="value==calculation.combined_with">
+                Probability of events A and B if independent: P(A)*P(B)
+            </p>
+            <p id="either" v-else>
+                Probability of event A or B occuring: P(A) + P(B) - P(A)*P(B)
+            </p>
+        </template>
+    </Card>
 </template>
 
 
 <script lang="ts">
     import { defineComponent } from "vue";
     import SelectButton from 'primevue/selectbutton'
+    import Card from 'primevue/card';
 
     interface Data {
         options: string[]
         value: string
+        calculation: {}
+    }
+
+    enum Calculation {
+        combined_with = "Combined With",
+        either = "Either",
     }
 
     export default defineComponent({
         name: 'App',
         components: {
             SelectButton,
+            Card
         },
         data(): Data {
             return {
-                options: ["Combined With", "Either"],
-                value: "Combined With"
+                options: [Calculation.combined_with, Calculation.either],
+                value: Calculation.combined_with,
+                calculation: Calculation
             }
         }
     })
--- redington.client/src/main.ts
+++ redington.client/src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import SelectButton from 'primevue/selectbutton'
-
+import Card from 'primevue/card';
 import App from './App.vue'
 
 import 'primevue/resources/themes/lara-light-green/theme.css'
@@ -10,4 +10,5 @@ import 'primevue/resources/themes/lara-light-green/theme.css'
 const app = createApp(App);
 app.use(PrimeVue);
 app.component('SelectButton', SelectButton);
+app.component('Card', Card);
 app.mount('#app');
