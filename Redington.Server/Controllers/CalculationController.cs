559b4a0e4284a22d8d6783a51d1a697e28245f76
Reivynn Chiang <reivynn.chiang@gmail.com>
2024-01-15 15:45:08 +0000

    Extracted a method to reduce code duplication

--- Redington.Server/Controllers/CalculationController.cs
+++ Redington.Server/Controllers/CalculationController.cs
@@ -3,6 +3,7 @@ using Redington.Server.Models;
 
 namespace Redington.Server.Controllers
 {
+    //This controller deals with the API in regards to Data and doing calculations
     [ApiController]
     [Route("[controller]")]
     public class CalculationController : ControllerBase
@@ -20,23 +21,25 @@ namespace Redington.Server.Controllers
         public IActionResult CombinedWith([FromBody] Data data)
         {
             double result = _service.CombinedWith(data);
-            if (result == -1)
-            {
-                return BadRequest(new { success = false });
-            } else
-            {
-                return Ok(new { success = true, val = result });
-            }
+            return response(result);
         }
+
+        
 
         [HttpPost]
         [Route("Either")]
         public IActionResult Either([FromBody] Data data)
         {
             double result = _service.Either(data);
+            return response(result);
+        }
+
+        //Helper method to reduce code duplication
+        private IActionResult response(double result)
+        {
             if (result == -1)
             {
-                return BadRequest(new { success = false});
+                return BadRequest(new { success = false });
             }
             else
             {
