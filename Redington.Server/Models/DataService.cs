ba3a09f1c0e1ed2aa6c8b8aa042ba45035b474a8
Reivynn Chiang <reivynn.chiang@gmail.com>
2024-01-15 15:44:52 +0000

    Added comments to dataservice for clarity

--- Redington.Server/Models/DataService.cs
+++ Redington.Server/Models/DataService.cs
@@ -3,10 +3,12 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Redington.Server.Models
 {
+    // This service class is the middleman between Data Model and Calculation Controller to keep thin controllers and models
     public class DataService
     {
         public bool IsValid(Data dataToValidate)
         {
+            // If A or B is null this will short circut and not cause a exception
             return dataToValidate.A != null && dataToValidate.B != null && dataToValidate.A >= 0 && dataToValidate.A <= 1 && dataToValidate.B >= 0 && dataToValidate.B <= 1;
         }
 
